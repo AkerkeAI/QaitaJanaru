a65250998fbd17c326d6bb76827eea08ae0d02a1
Akerke <urynbasarakerke409@gmail.com>
2026-08-22 00:06:24 +0500

    type error fix sstyle

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -458,7 +458,6 @@ export default function ProfilePage() {
                   </div>
                 </div>
               </div>
-            </div>
               <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
                 {[
                   {
